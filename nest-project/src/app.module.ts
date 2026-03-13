76d1d76f6b33171fa8c16b6afb0649e55cd43e01
CerealXVI <hasinduvithanage@gmail.com>
2026-03-13 15:10:23 +1100

    6.5

--- nest-project/src/app.module.ts
+++ nest-project/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [BooksModule],
+  imports: [BooksModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
