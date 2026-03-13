1e96bca6ab3827fdcdb9eb2f529f292a32ac2801
CerealXVI <hasinduvithanage@gmail.com>
2026-03-13 15:55:13 +1100

    Completed Milestone 6

--- nest-project/src/app.module.ts
+++ nest-project/src/app.module.ts
@@ -3,10 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books.module';
 import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm'; 
 
 @Module({
-  imports: [BooksModule, ProductsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST ?? 'localhost',
+      port: parseInt(process.env.DB_PORT ?? '5432'),
+      username: process.env.DB_USER ?? 'myuser',
+      password: process.env.DB_PASSWORD ?? 'mysecretpassword',
+      database: process.env.DB_NAME ?? 'mydb',
+      autoLoadEntities: true,
+      synchronize: true,    }),
+    BooksModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
